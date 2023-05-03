7c530985a0de87ec3364bffe3870e898f303aa13
chao <2417940035@qq.com>
2023-05-03 18:21:24 +0800

    基础Url对应方法

---
+++ src/main/java/com/example/demo/service/jyc/impls/AdminServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.demo.service.jyc.impls;
+
+import com.example.demo.service.jyc.inters.AdminService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @ClassName AdminServiceImpl
+ * @Description
+ * @Author chao
+ * @Date 2023/5/3
+ * @Version 1.0
+ **/
+@Service("adminService")
+public class AdminServiceImpl implements AdminService {
+}
